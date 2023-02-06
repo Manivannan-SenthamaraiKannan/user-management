32dea856d4684965bae6a2be41e3efa62619bff2
Manivannan S <sharvamanivannan@gmail.com>
2023-02-06 10:23:39 +0000

    Sidebar, Navbar & Topbar components updates.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Topbar from './Dashboard/Topbar/Topbar'
+import Sidebar from './Dashboard/Sidebar/Sidebar'
+
+function App() {
+  return (
+    <div className="App">
+      <Topbar />
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
